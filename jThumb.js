5ecb4b09af1367ca32fa9110bbd64bd4f7c48012
Josema Enzo <josema@synchtank.net>
2013-07-25 01:25:19 +0100

    Making getConfig as heritable

--- jThumb.js
+++ jThumb.js
@@ -25,6 +25,7 @@
 		else if (options.onError)
 			options.onError($.jThumb.error[0], cElem, iElem);
 
+
         function getRealSizeImage(src)
         {
 			var img = new Image();
@@ -37,16 +38,18 @@
 				keepGoing();
 			}
         }
-		
+
+
 		function keepGoing()
         {		
-			var args;
-			if (args = getArguments( $(iElem).attr( options.attrName ) )) {}
-			else if (args = getArguments( $(cElem).attr( options.attrName ) )) {}
-			else args = getArguments( options.position );
+			var args = [
+				getArguments( options.align ),
+				getArguments( $(cElem).attr( options.attrName )),
+				getArguments( $(iElem).attr( options.attrName ))
+			];
 
-			
-			console.log(getValues(args));
+			var config = getConfig(args);
+			console.log(args);
         }
 		
 		
@@ -60,63 +63,95 @@
 				var regresult;
 				for (var i=0; i<cut.length; ++i)
 				{
-					console.log(i, cut[i]);
 					regresult = $.jThumb.const.regexp.exec(cut[i]);
-					console.log(regresult);
+
 					if (regresult != undefined && regresult != null && regresult[1] != undefined)
 					{
 						args[n] = [(regresult[1]).toLowerCase()];
 						if (regresult[4] != undefined)
-							args[n][1] = regresult[4] + ((regresult[5] == undefined) ? options.standardUnit : regresult[5]);
+							args[n][1] = [regresult[4], ((regresult[5] == undefined) ? options.standardUnit : regresult[5])];
 						
 						n += 1;
 					}
-					console.log(cut.length, args.length);
 				}
-				console.log("----------------");
 			}
 			return (args.length>0) ? args : false;
         }
-		
-		function getValues(args)
+
+
+		function getConfig(argslist)
         {
-			var cut = jQuery.trim($.jThumb.defaults.position).split(" ");
+			var cut = jQuery.trim($.jThumb.defaults.align).split(" ");
 			var values = {
-				positionX: cut[0],
-				positionY: cut[1],
+				alignX: cut[0],
+				alignY: cut[1],
+				paddingX: 0+options.standardUnit,
+				paddingY: 0+options.standardUnit,
 				allowScale: options.allowScale,
 				allowZoom: options.allowZoom
 			};
 			
-			var xsetted, ysetted = false;
-			for (var i=0; i<args.length; ++i)
-			{
-				if (args[i][0] == "left" || args[i][0] == "right")
-				{
-					values.positionX = args[i][0];
-					xsetted = true;
-				}
-				else if (args[i][0] == "top" || args[i][0] == "bottom")
-				{
-					values.positionY = args[i][0];
-					ysetted = true;
-				}
-				else if (args[i][0] == "center")
-				{
-					if (!xsetted)
-					{
-						xsetted = true;
-						values.positionX = args[i][0];
-					}
-					else (!ysetted)
-						values.positionY = args[i][0];
-				}
-				else if (args[i][0] == "allowscale")
-					values.allowScale = true;
-				else if (args[i][0] == "allowzoom")
-					values.allowZoom = true;
-			}
 			
+			var j, i, args, alignX, alignY, centeri, center;
+			for (j=0; j<argslist.length; ++j)
+			{
+				alignX = alignY = null;
+				centeri = 0;
+				center = [];
+				args = argslist[j];
+
+				for (i=0; i<args.length; ++i)
+				{
+					if (args[i][0] == "left" || args[i][0] == "right")
+						alignX = args[i];
+	
+					else if (args[i][0] == "top" || args[i][0] == "bottom")
+						alignY = args[i];
+	
+					else if (args[i][0] == "center")
+						center[centeri++] = args[i];
+	
+					else if (args[i][0] == "allowscale")
+						values.allowScale = true;
+	
+					else if (args[i][0] == "allowzoom")
+						values.allowZoom = true;
+				}
+				
+				if (alignX != undefined)
+					values.alignX = alignX;
+				if (alignY != undefined)
+					values.alignY = alignY;
+	
+				if (alignX == undefined && center.length > 0)
+					values.alignX = center[center.length-1];
+				if (alignY == undefined)
+				{
+				 	if ((center.length==1 && alignX!=undefined) || (center.length>1))
+				 	{
+						values.alignY = center[center.length-1];
+						if (values.alignX[0] == "center" && (center.length>1))
+							values.alignY = center[center.length-2];
+					}
+				}
+				
+				if (typeof values.alignX != "string" && values.alignX[0] != undefined)
+				{
+					if (values.alignX[1] != undefined)
+						values.paddingX = values.alignX[1][0] + (((values.alignX[1][1] != undefined)) ? values.alignX[1][1] : options.standardUnit);
+	
+					values.alignX = values.alignX[0];
+				}
+				if (typeof values.alignY != "string" && values.alignY[0] != undefined)
+				{
+					if (values.alignY[1] != undefined)
+						values.paddingY = values.alignY[1][0] + (((values.alignY[1][1] != undefined)) ? values.alignY[1][1] : options.standardUnit);
+	
+					values.alignY = values.alignY[0];
+				}
+				console.log(values);
+			}
+
 			return values;
 		}
     };
@@ -124,7 +159,7 @@
 
     $.jThumb.defaults = {
 		img: "> img",
-		position: "AAAA BBBB",
+		align: "AAAA BBBB",
 		allowScale: false,
 		allowZoom: false,
 		onError: null,
@@ -143,16 +178,8 @@
 	
 	
 	$.jThumb.const = {
-		regexp: /(center|left|top|right|bottom|allowscale|allowzoom)(\((([-0-9]+)(%|px|pt|em|cm|in)?)?\))?/gi
+		regexp: /(center|left|top|right|bottom|allowscale|allowzoom)(\((([-0-9]+)(%|px|pt|em|cm|in)?)?\))?/i
 	};
 
 })(jQuery);
-
-
-
-
-$(document).ready(function() {
-    $(".thumb").jThumb({position: "left(25px) center(-12%)", onError: function(a,b,c){alert(a)}});
-});
-
 
